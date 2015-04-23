1d66df7cb0c8ac07e36983bf660743523e1763f1
Hemen Chhatbar <hemenc@slalom.com>
2015-04-22 23:31:47 -0700

    proxy-server completed!

---
+++ index.js
@@ -0,0 +1,41 @@
+let http = require('http')
+let fs = require('fs')
+let through = require('through')
+let request = require('request')
+let argv = require('yargs')
+	.default('host', '127.0.0.1')
+	.argv
+let scheme = 'http://'
+let port = argv.port || argv.host === '127.0.0.1' ? 7000 : 80
+let destinationUrl = argv.url || scheme + argv.host + ':' + port
+let logStream = argv.logfile ? fs.createWriteStream(argv.logfile) : process.stdout
+console.log(logStream)
+
+http.createServer((req,res) => {
+	logStream.write('Echo request: \n' + JSON.stringify(req.headers))
+	for(let header in req.headers){
+		res.setHeader(header,req.headers[header])
+	}
+	through(req, logStream, {autoDestroy:false})
+	req.pipe(res)
+}).listen(7000)
+
+logStream.write('Listening at http://127.0.0.1:7000')
+
+http.createServer((req,res) => {
+	let options = {
+		headers : req.headers,
+		url : destinationUrl
+	}
+	logStream.write('Proxy request: \n' + JSON.stringify(req.headers))
+	through(req, logStream, {autoDestroy:false})
+	let destinationResponse = req.pipe(request(options))
+
+		logStream.write(JSON.stringify(destinationResponse.headers))
+		destinationResponse.pipe(destinationResponse)
+	through(req, logStream, {autoDestroy:false})
+	
+	
+	//
+	
+}).listen(7001)
